b70a4f03d572c0477c92e7eba9fbe9fccac9cb73
foziljonkholmatov <fozil77739@gmail.com>
2025-08-04 23:19:17 +0500

    translate add

--- pages/views.py
+++ pages/views.py
@@ -3,11 +3,40 @@ from django.shortcuts import render, redirect
 from pages.forms import ContactModelForm
 
 
+def none_page_view(request):
+    return render(request, 'pages/404.html')
+
+
 def home_page_view(request):
     return render(request, 'home.html')
 
+
 def checkout_view(request):
     return render(request, 'pages/checkout.html')
+
+
+def about_page_view(request):
+    return render(request, 'pages/about-us.html')
+
+
+def product_details_view(request):
+    return render(request, 'shop/product-details.html')
+
+
+def add_cart(request):
+    return render(request, 'shop/shopping-cart.html')
+
+
+def wishlist_view(request):
+    return render(request, 'pages/wishlist.html')
+
+
+def shop_list_view(request):
+    return render(request, 'shop/shop.html')
+
+
+def blogs_view(request):
+    return render(request, 'pages/blogs.html')
 
 def contact_pages_view(request):
     if request.method == 'POST':
